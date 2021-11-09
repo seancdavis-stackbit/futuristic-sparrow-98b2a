7085d62dd81179158b3cdaf10fccc79cf666c3f8
Stackbit Code Editor <projects@stackbit.com>
2021-11-09 15:52:41 +0000

    AlertSection.jsx: changed

--- src/components/AlertSection.jsx
+++ src/components/AlertSection.jsx
@@ -1,5 +1,5 @@
 const AlertSection = ({ annotationPrefix, body }) => {
-    return <div data-sb-field-path={}>
+    return <div data-sb-field-path={annotationPrefix}>
         <p>{body}</p>
     </div>
 }
