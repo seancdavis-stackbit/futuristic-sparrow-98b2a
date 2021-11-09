d098656f5725671f2e666ac71278055cc240f339
Stackbit Code Editor <projects@stackbit.com>
2021-11-09 16:32:00 +0000

    AlertSection.jsx: removed

--- src/components/AlertSection.jsx
+++
@@ -1,7 +0,0 @@
-const AlertSection = ({ annotationPrefix, body }) => {
-    return <div data-sb-field-path={annotationPrefix} className="">
-        <p data-sb-field-path=".body">{body}</p>
-    </div>
-}
-
-export default AlertSection
