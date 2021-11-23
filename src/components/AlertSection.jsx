edf935eeb96230e6f88c522e788ab278274d897a
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 19:09:38 +0000

    AlertSection.jsx: changed

--- src/components/AlertSection.jsx
+++ src/components/AlertSection.jsx
@@ -1,5 +1,13 @@
+import React from 'react'
+
 const AlertSection = ({ body }) => {
-    return <p>{body}</p>
+    return (
+        <div className="py-6 bg-primary">
+            <div className="container mx-auto">
+                <p className="text-center text-xl text-white">{body}</p>
+            </div>
+        </div>
+    )
 }
 
 export default AlertSection
